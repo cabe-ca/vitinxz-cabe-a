94b1d94a1b898d5338775804574073cea98ecbca
cabe-ca <vitorhugogasparini2007@gmail.com>
2024-11-19 21:55:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'shitpost',
+    'O que é Meme?',
+    'Figuras engraçadas'
+)
+
+criaCartao(
+    'shitpost2',
+    'quem é manuel gomes?',
+    'Caneta Azul, Azul caneta'
+)
+
+criaCartao(
+    'shitpost3',
+    'o que é a sarada que o winderson levou?',
+    'falta de respeito daquele indiano FDP do CRLH'
+)
+
+criaCartao(
+    'shitpost supremo',
+    'mike tyson?',
+    'homem velho que apanhou'
+)
